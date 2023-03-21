915da7c1dbea856dcee1804d5d70f30e39a11f52
chendeli <419287484@qq.com>
2023-03-21 12:14:26 +0800

    配置

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2022-04-15 16:53:53
- * @LastEditTime: 2022-07-25 15:01:19
+ * @LastEditTime: 2023-03-21 12:08:18
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: electron-forge打包配置
@@ -13,6 +13,7 @@ module.exports = {
     // extraResource: ['./assets/Readme.txt', './assets/img/a.png'], // 静态文件
     icon: './assets/icon',
     asar: true, // 以asar的方法加密，以防源码泄露
+    osxSign: {} //IOS object must exist even if empty
   },
 
   makers: [
